3a533366f57eba633a49f2e5c5a5630dce864482
Matias Imoff <matias.c.imoff@gmail.com>
2025-07-03 23:28:20 -0300

    Modificación de CV 03-07-2025

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
+const menu = document.querySelector("#menu-icon"); // Acá estamos seleccionando con el querySelección por ID
+const navList = document.querySelector(".navlist"); // Acá estamos seleccionando con el querySelección por Clase
 
-
-const menu = document.querySelector('#menu-icon') // Acá estamos seleccionando con el querySelección por ID
-const navList = document.querySelector('.navlist') // Acá estamos seleccionando con el querySelección por Clase
-
-menu.onclick = () => { //Para que escuche el evento.
-    menu.classList.toggle('bx-x') //Creamos clases para dales estilo en el style
-    navList.classList.toggle('open') //Creamos clases para dales estilo en el style
-    
-
-}
+menu.onclick = () => {
+  //Para que escuche el evento.
+  menu.classList.toggle("bx-x"); //Creamos clases para dales estilo en el style
+  navList.classList.toggle("open"); //Creamos clases para dales estilo en el style
+};
 
 const sr = ScrollReveal({
-    distance: '65px',
-    duration: 2000,
-    delay: 450,
-    reset: true
-})
+  distance: "65px",
+  duration: 2000,
+  delay: 450,
+  reset: true,
+});
 
-sr.reveal('.hero-text', {delay: 200, origin: 'top'})
-sr.reveal('.hero-img', {delay: 450, origin: 'top'})
-sr.reveal('.icons', {delay: 500, origin: 'left'})
-sr.reveal('.scroll-down', {delay: 450, origin: 'right'})
+sr.reveal(".hero-text", { delay: 200, origin: "top" });
+sr.reveal(".hero-img", { delay: 350, origin: "top" });
+sr.reveal(".icons", { delay: 500, origin: "left" });
+sr.reveal(".scroll-down", { delay: 450, origin: "right" });
 
 function scrollToTop() {
-    const container = document.getElementById('scrollContainer');
-    container.scrollTo({
-        top: 0,
-        behavior: 'smooth' // Hacer el scroll suave
-    });
+  const container = document.getElementById("scrollContainer");
+  container.scrollTo({
+    top: 0,
+    behavior: "smooth", // Hacer el scroll suave
+  });
 }
